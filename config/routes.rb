2a166c1d69245fb2140f02a8ce3747b4fc176b99
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-10-12 12:42:35 -0300

    estrutura inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :produtos
+  root to: 'home#index'
+  resources :tipo_produtos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # match '/home' => 'home#index', via: :get
+
+
 end
