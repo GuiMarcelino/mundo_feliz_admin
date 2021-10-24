c3a5fc4685f3862b160e3e3eb64750d9aa29988c
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-10-24 20:16:11 -0300

    add column

---
+++ db/migrate/20211024230522_add_column_value_to_products.rb
@@ -0,0 +1,5 @@
+class AddColumnValueToProducts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :produtos, :valor, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_134315) do
+ActiveRecord::Schema.define(version: 2021_10_24_230522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_134315) do
     t.bigint "tipo_produto_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "valor"
     t.index ["tipo_produto_id"], name: "index_produtos_on_tipo_produto_id"
   end
 
